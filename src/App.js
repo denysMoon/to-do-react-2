c09f3c5c9083910cbd538693f3b02a4c861f675b
denysMoon <denysulvanskyi@gmail.com>
2021-08-27 15:28:39 +0300

    start work with app. add starting structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles/App.css';
+
+import PostItem from './components/PostItem/PostItem'
+
+function App() {
+  return (
+    <div className="App">
+      <h3>React JS</h3>
+      <PostItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PostItem/PostItem.jsx
@@ -0,0 +1,15 @@
+const PostItem = ( props ) =>{
+    return(
+        <div className="post">
+            <div className="post-content">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident rem saepe totam velit voluptatibus adipisci fuga!
+                Unde in architecto expedita nobis, corporis minima! Repellat, possimus eligendi quasi repellendus tempore ipsam.
+            </div>
+            <div className="post-button">
+                <button>Button</button>
+            </div>
+        </div>
+    )
+}
+
+export default PostItem
